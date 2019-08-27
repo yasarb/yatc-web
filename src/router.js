0b09e4f2e3c7bfaf47dfbb97721b4b41323344a0
yasar budulgan <yb.budulgan@gmail.com>
2019-08-27 22:16:18 +0300

    Add new routes

--- src/router.js
+++ src/router.js
@@ -16,10 +16,22 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: () => import('./views/Notifications.vue'),
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: () => import('./views/Messages.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('./views/Profile.vue'),
     },
   ],
 });
