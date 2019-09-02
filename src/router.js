52c3562088686251af49b7bd57d0bc0f76d206fd
yasar budulgan <yb.budulgan@gmail.com>
2019-09-02 22:50:22 +0300

    Add navigation guards

--- src/router.js
+++ src/router.js
@@ -1,29 +1,50 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import store from './store/index';
 
 Vue.use(Router);
 
-export default new Router({
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('signin');
+};
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
+      alias: '/home',
       component: Home,
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/signin',
       name: 'signin',
       component: () => import('./views/Signin.vue'),
       meta: { hideNavigation: true },
+      beforeEnter: ifNotAuthenticated,
     },
     {
       path: '/signup',
       name: 'signup',
       component: () => import('./views/Signup.vue'),
       meta: { hideNavigation: true },
+      beforeEnter: ifNotAuthenticated,
     },
     {
       path: '/about',
@@ -34,11 +55,13 @@ export default new Router({
       path: '/notifications',
       name: 'notifications',
       component: () => import('./views/Notifications.vue'),
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/messages',
       name: 'messages',
       component: () => import('./views/Messages.vue'),
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/profile',
@@ -47,3 +70,5 @@ export default new Router({
     },
   ],
 });
+
+export default router;
