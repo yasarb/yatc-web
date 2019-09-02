6f2c6f1f7f05dea6e69a747a4e9727959d67eff4
yasar budulgan <yb.budulgan@gmail.com>
2019-09-02 22:52:13 +0300

    Reorganize auth store module

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
+import axios from 'axios';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
 import router from './router';
-import store from '@/store/index';
+import store from './store/index';
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
+const token = localStorage.getItem('access-token');
+
+if (token) {
+  axios.defaults.headers.common.Authorization = token;
+}
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import state from './state';
-// import getters from './getters';
-import actions from './actions';
-import mutations from './mutations';
+import auth from './modules/auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state,
-  //  getters,
-  actions,
-  mutations,
+  modules: {
+    auth,
+  },
 });
